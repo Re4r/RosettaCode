bc03529b37a4df2a0615323536ad07bf5cf74d9a
KirillMar <re4r@bk.ru>
2022-12-17 20:00:44 +0300

    seven

---
+++ AckermannFunction.js
@@ -0,0 +1,10 @@
+function ack(m, n) {
+    return (m === 0)
+        ? n + 1
+        : (m > 0 && n === 0)
+            ? ack(m - 1, 1)
+            : ack(m - 1, ack(m, n - 1)); 
+
+};
+
+console.log(ack(1, 1));
