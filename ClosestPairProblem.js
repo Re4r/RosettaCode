8fc9b723f8d62a212c5d4d6e52fecbce73294e53
KirillMar <re4r@bk.ru>
2022-12-18 20:31:05 +0300

    sixteen

---
+++ ClosestPairProblem.js
@@ -0,0 +1,14 @@
+const Point = function(x, y) {
+    this.x = x;
+    this.y = y;
+};
+
+Point.prototype.getX = function() {
+    return this.x;
+};
+
+Point.prototype.getY = function() {
+    return this.y;
+};
+
+
